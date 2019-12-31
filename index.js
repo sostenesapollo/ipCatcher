e065f712bbbfccc219f9283ba578ca7711abed97
sostenesapollo12 <sostenesapollo25@gmail.com>
2019-12-31 15:12:08 -0300

    changes

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log("ok");
+var http = require("http");
+
+init();
+
+function init() {
+  findIp().then(ip => {
+    saveOnDb(ip).then(() => {
+      timer();
+    });
+  });
+}
+
+function timer() {
+  setTimeout(() => {
+    init();
+  }, 30000);
+}
+
+function findIp() {
+  return new Promise((resolve, reject) => {
+    http.get("http://bot.whatismyipaddress.com?see=1", function(res) {
+      res.setEncoding("utf8");
+      res.on("data", function(chunk) {
+        resolve(chunk);
+      });
+    });
+  });
+}
+
+function saveOnDb(ip) {
+  return new Promise((resolve, reject) => {
+    http.get(
+      "http://minhasprovasonline.aguiaexpresspi.com.br?ip=" + ip,
+      function(res) {
+        res.setEncoding("utf8");
+        resolve(true);
+      }
+    );
+  });
+}
